02976b4900d93109531ad09f31329c2e23fa5df5
Silvia Mena <silvia1enero@gmail.com>
2025-04-15 21:47:29 +0200

    2.3 terminado y comprobado, falta ejercicio

--- 2.3.rebotes/plantilla/rebotes.vhd
+++ 2.3.rebotes/plantilla/rebotes.vhd
@@ -15,28 +15,23 @@ ARCHITECTURE rebotes_arq OF rebotes IS
   SIGNAL biestables: STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";              --input flip flops
   SIGNAL reinicia  : STD_LOGIC := '0';                                  --sync reset to zero
   SIGNAL cuenta    : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0'); --counter output
-  SIGNAL reset     : STD_LOGIC := '0';
 BEGIN
 
   reinicia <= biestables(0) xor biestables(1);   --determina cuando se reinicia o cuenta el contador
-
-
+  
   PROCESS(clk)
   BEGIN
-    IF(clk'EVENT and clk = '1') THEN -- si hay rising_edge
+    IF(clk'EVENT and clk = '1') THEN
       biestables(0) <= i;
-      biestables(1) <= biestables(0);
-      reset <= reinicia;
-      IF(reinicia = '1') THEN
-        CUENTA <= (others =>'0');
-      ELSE
-        CUENTA <= CUENTA + 1;
-        IF(CUENTA(N-1) = '1') THEN
-            o <= biestables(1);
-        END IF;
-        END IF;
-      END IF;
 
+      -- rellenar aquí
 
   END PROCESS;
 END rebotes_arq;
+
+
+
+
+
+
+
---
+++ 2.3.rebotes/rebotes_tbComprobacion.vhd
@@ -0,0 +1,96 @@
+-- **********************************************************************
+-- LIBRERIAS
+-- **********************************************************************
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+-- **********************************************************************
+-- ENTIDAD     (entradas/salidas, el fichero de simulación no tiene)
+-- **********************************************************************
+ENTITY test_rebotes IS
+END    test_rebotes;
+
+-- **********************************************************************
+-- ARQUITECTURA   (descripción de los estímulos)
+-- **********************************************************************
+ARCHITECTURE test_rebotes_arq OF test_rebotes IS
+    --Declaración de componentes
+    COMPONENT rebotes
+		  GENERIC(
+			N : INTEGER := 3  -- Tamaño del contador (ajustable segun la frecuencia del reloj)
+		  );
+		  PORT(
+			clk     : IN  STD_LOGIC;  -- Reloj del sistema
+			i       : IN  STD_LOGIC;  -- Entrada del botón con rebote
+			o       : OUT STD_LOGIC   -- Salida debounced
+		  );
+    END COMPONENT;
+
+    SIGNAL CLK,I,O : std_logic;
+    constant ciclo: time :=  10 ns;  -- hay que dejar un espacio entre 10 y ns
+BEGIN
+    -- ///////////////////////////////////////////////////////////////////////////////
+    -- Se crea el componente U1 y se conecta a las señales internas de la arquitectura
+    -- ///////////////////////////////////////////////////////////////////////////////
+    i_rebotes: rebotes   
+                 GENERIC MAP (N => 2)
+                 PORT    MAP(
+                                clk    => CLK,
+                                i      => I,
+                                o      => O
+                             );
+
+    -- ======================================================================
+    -- Proceso de la entrada de reloj. Se ejecuta indefinidamente ya que no tiene "WAIT;"
+    -- ======================================================================
+    PROCESS
+    BEGIN
+        CLK<='0';    wait for ciclo/2;
+        CLK<='1';    wait for ciclo/2;
+    END PROCESS;
+
+
+    -- ======================================================================
+    -- Proceso para el resto de entradas; ENABLE_test
+    -- ======================================================================
+    tb: PROCESS
+    BEGIN
+        I <= '0';
+        wait for 5.75*ciclo;
+        i <= '1';
+        wait for 0.5*ciclo;
+        i <= '0';
+        wait for 2*ciclo;
+        i <= '1';
+        wait for 1*ciclo;
+        i <= '0';
+        wait for 2*ciclo;
+        i <= '1';
+        
+
+        
+
+         wait;                  -- Espera indefinida
+    end process tb;
+
+END test_rebotes_arq;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
